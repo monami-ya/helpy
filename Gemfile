fb5dde0c77eb7d400531f56df0fe2c57e6ed37ee
Masaki Muranaka <monaka@monami-ya.com>
2017-02-01 08:17:35 +0000

    Use Azure Blob as CarrierWave's storage.

--- Gemfile
+++ Gemfile
@@ -88,6 +88,7 @@ gem 'cloudinary', '1.1.2'
 gem 'attachinary'
 
 gem 'carrierwave', '~> 1.0.0'
+gem 'carrierwave-azure'
 gem "jquery-fileupload-rails"
 gem 'mini_magick'
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,6 @@
+
+CarrierWave.configure do |config|
+  config.azure_storage_account_name = ENV['AZURE_STORAGE_ACCOUNT_NAME']
+  config.azure_storage_access_key = ENV['AZURE_STORAGE_ACCESS_KEY']
+  config.azure_container = ENV['AZURE_STORAGE_CONTAINER_NAME']
+end
