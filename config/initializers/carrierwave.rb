dd19bc48f11cd267322303611648c8cbbe1b29c3
Masaki Muranaka <monaka@monami-ya.com>
2017-02-03 03:44:31 +0000

    Force to use https.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,6 @@
 CarrierWave.configure do |config|
   config.azure_storage_account_name = ENV['AZURE_STORAGE_ACCOUNT_NAME']
   config.azure_storage_access_key = ENV['AZURE_STORAGE_ACCESS_KEY']
+  config.azure_storage_blob_host = "https://#{ENV['AZURE_STORAGE_ACCOUNT_NAME']}.blob.core.windows.net"
   config.azure_container = ENV['AZURE_STORAGE_CONTAINER_NAME']
 end
